80b83528e9554672defe689f08a46600661c9fda
ggjack <gg.jack@hotmail.com>
2017-01-21 16:18:46 -0500

    Added Upload(doesnt work) and go back button. Also hide and showed parts when needed

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+
 import Webcam from './react-webcam';
 import snap from './../styles/logo/camera.svg';
 
@@ -9,7 +11,9 @@ export default class Camera extends Component {
             screenshot : null
         }
         this.snap = this.snap.bind(this);
+        this.clearSnap = this.clearSnap.bind(this);
     }
+
     snap(){
         var screenshot = this.refs.webcam.getScreenshot();
         console.log(screenshot);
@@ -17,14 +21,37 @@ export default class Camera extends Component {
             screenshot
         })
     }
+
+    clearSnap(){
+        this.setState({
+            screenshot: null
+        });
+    }
+
     render(){
+        const theWebcam=(
+        <div>
+            <Webcam ref="webcam" screenshotFormat='image/jpeg' />
+                <button onClick={this.snap}>
+                    <img src={snap} className="snap" alt="logo"/>
+                </button>
+        </div>
+        )
+
+        const toUpload=(
+            <div>
+                <img src={this.state.screenshot} />
+                <div>
+                    <button> Upload </button>
+                    <button onClick={this.clearSnap}> Go Back </button>
+                </div>
+            </div>
+        )
+
         return(
             <div>
-                <Webcam ref="webcam"/>
-                <div>
-                    <img onClick={this.snap} src={snap} className="snap" alt="logo"/>
-                </div>
-                {this.state.screenshot ? <img src={this.state.screenshot} /> : null }
+                {!this.state.screenshot ? theWebcam : null}
+                {this.state.screenshot ?  toUpload : null }
             </div>
         )
     }
