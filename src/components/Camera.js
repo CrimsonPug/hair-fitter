9fd0ddc9f9d2d12481c09ceb0f6774fc78ece594
CrimsonPug <ikram.tuan.mustapha@gmail.com>
2017-01-21 17:21:23 -0500

    camera update

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Webcam from './react-webcam';
 import snap from './../styles/logo/camera.svg';
+import trash from './../styles/logo/trash.svg';
 
 export default class Camera extends Component {
     constructor(){
@@ -9,9 +10,17 @@ export default class Camera extends Component {
             screenshot : null
         }
         this.snap = this.snap.bind(this);
+        this.delete = this.delete.bind(this);
+    }
+    delete(){
+        console.log('delete');
+        this.setState({
+            screenshot: null
+        })
     }
     snap(){
         var screenshot = this.refs.webcam.getScreenshot();
+        
         console.log(screenshot);
         this.setState({
             screenshot
@@ -20,11 +29,16 @@ export default class Camera extends Component {
     render(){
         return(
             <div>
-                <Webcam ref="webcam"/>
+                
                 <div>
                     <img onClick={this.snap} src={snap} className="snap" alt="logo"/>
                 </div>
-                {this.state.screenshot ? <img src={this.state.screenshot} /> : null }
+                {this.state.screenshot ? 
+                    <div>
+                        <img src={this.state.screenshot} /> 
+                        <img onClick={this.delete} src={trash} className="snap" alt="logo"/>
+                        </div> 
+                        : <Webcam ref="webcam" screenshotFormat='image/jpeg'/> }
             </div>
         )
     }
